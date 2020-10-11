8a001a12cfc431d024a6cbd288eca05d330d95c7
aleksandarm03 <accam003@gmail.com>
2020-10-11 15:36:49 +0200

    update: 11.10.2020.

---
+++ Biblioteka/Biblioteka/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+       public abstract class Primerak
+    {
+        protected string naziv;
+        protected int god;
+
+        public Primerak(string naziv, int god)
+        {
+            if (naziv.Equals(string.Empty))
+                throw new Exception("Nema naziv!");
+            else if (god < 1990 || god > 2019) //tekuca godina je 2019
+                throw new Exception("Neispravna godina!");
+            this.naziv = naziv;
+            this.god = god;
+        }
+
+        public bool StarijiOd(Primerak p)
+        {
+            return god < p.god;
+        }
+        public bool MladjiOd(Primerak p)
+        {
+            return god > p.god;
+        }
+
+        public abstract string Info();
+    }
+
+    public class Knjiga : Primerak
+    {
+        private string autor;
+
+        public Knjiga(string naziv, string autor, int god) : base(naziv, god)
+        {
+            if (autor.Equals(string.Empty)) throw new Exception("Nema autora!");
+            this.autor = autor;
+        }
+
+        public override string Info()
+        {
+            string s = naziv + "-" + autor;
+            s += ", god: " + god;
+            return s;
+        }
+    }
+
+    public class Casopis : Primerak
+    {
+        private int mesec;
+
+        public Casopis(string naziv, int god, int mesec) : base(naziv, god)
+        {
+            if (mesec < 1 || mesec > 12)
+                throw new Exception("Neispravan mesec!");
+            this.mesec = mesec;
+        }
+
+        public override string Info()
+        {
+            return naziv + ", god: " + god + ", mesec: " + mesec;
+        }
+    }
+
+}
---
+++ Biblioteka/Biblioteka/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Primerak[] p = {
+                (Primerak)(new Knjiga("Bas vam hvala", "Marko Vidojkovic", 2018)),
+                (Primerak)(new Knjiga("Na zapadu nista novo", "Remark", 2015)),
+                (Primerak)(new Casopis("Nacionalna geografija", 2018, 1)),
+                (Primerak)(new Knjiga("Trijumfalna kapija", "Remark", 2015)),
+                (Primerak)(new Casopis("Nacionalna geografija", 2019, 2)),
+                (Primerak)(new Casopis("Nacionalna geografija", 2017, 3)),
+                (Primerak)(new Casopis("Nacionalna geografija", 2017, 4)),
+                (Primerak)(new Casopis("Nacionalna geografija", 2019, 5)),
+                (Primerak)(new Casopis("Nacionalna geografija", 2019, 6)),
+                (Primerak)(new Knjiga("Programiranje - klase i objekti", "Dusa Vukovic", 2018))
+            };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            listBox1.Items.Clear();
+            for (int i = 0; i < p.Length; i++)
+            {
+                for (int j = i + 1; j < p.Length; j++)
+                {
+                    if (p[j].StarijiOd(p[i]))
+                    {
+                        Primerak pom = p[i];
+                        p[i] = p[j];
+                        p[j] = pom;
+                    }
+                }
+            }
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (p[i] is Knjiga)
+                    listBox1.Items.Add("KNJIGA: " + p[i].Info());
+                else if (p[i] is Casopis)
+                    listBox1.Items.Add("CASOPIS: " + p[i].Info());
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton2.Checked = false;
+            listBox1.Items.Clear();
+            for (int i = 0; i < p.Length; i++)
+            {
+                for (int j = i + 1; j < p.Length; j++)
+                {
+                    if (p[j].MladjiOd(p[i]))
+                    {
+                        Primerak pom = p[i];
+                        p[i] = p[j];
+                        p[j] = pom;
+                    }
+                }
+            }
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (p[i] is Knjiga)
+                    listBox1.Items.Add("KNJIGA: " + p[i].Info());
+                else if (p[i] is Casopis)
+                    listBox1.Items.Add("CASOPIS: " + p[i].Info());
+            }
+        }
+
+        }
+    }
+
